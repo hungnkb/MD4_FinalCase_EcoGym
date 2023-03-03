abffaeab9cd644382e6fa4c1bd1e0e3cbaf996da
hungnkb <hungnkb@gmail.com>
2023-03-03 23:25:20 +0700

    delete wallet.router

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -2,13 +2,11 @@ import apiRouter from "./api.router";
 import authRouter from "./auth.router";
 import homeRouter from "./home.router";
 import authorize from "../middleware/authorize";
-import walletRouter from "./wallet.router";
 
 const route = (app) => {
   app.use("/", homeRouter);
   app.use("/auth", authRouter);
   app.use("/api", apiRouter);
-  // app.use("/wallet", walletRouter);
 };
 
 export default route;
