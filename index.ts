0bbeb8da0350d0fed6b984872990f0c62a5c3d0b
hungnkb <hungnkb@gmail.com>
2023-03-03 11:04:01 +0700

    edit link db

--- index.ts
+++ index.ts
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 mongoose.set('strictQuery', true);
-mongoose.connect('mongodb://127.0.0.1:27017/ecogym')
+mongoose.connect('mongodb+srv://hungnkb:1993hung@cluster0.zbfd8ti.mongodb.net/?retryWrites=true&w=majority')
   .then(() => console.log('DB Connected!'));
 
 app.set('view engine', 'ejs');
