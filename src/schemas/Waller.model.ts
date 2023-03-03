21e0f59833473ac57e81401b197d099a46d86eae
hungnkb <hungnkb@gmail.com>
2023-03-03 20:48:58 +0700

    api create wallet done

--- src/schemas/Waller.model.ts
+++ src/schemas/Waller.model.ts
@@ -1,18 +1,19 @@
-import { Schema, model } from "mongoose";
+import { Schema, model, ObjectId, Types } from "mongoose";
+import User from "./User.model";
 
 interface IWallet {
     idUser: string,
     walletName: string,
     icon: string,
-    totalMoneyLeft: number
+    totalMoneyLeft: number,
 }
 
 
 const walletSchema = new Schema<IWallet>({
-    idUser: String,
+    idUser: String, 
     walletName: String,
     icon: String,
-    totalMoneyLeft: Number
+    totalMoneyLeft: Number,
 })
 
 const Wallet = model<IWallet>('Wallet', walletSchema);
