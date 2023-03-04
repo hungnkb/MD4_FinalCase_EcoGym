e0a080f22dbcf347da92bd7292087c475e79b158
hungnkb <hungnkb@gmail.com>
2023-03-04 20:28:06 +0700

    update register re-password

---
+++ src/controllers/api/statics.api.controller.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+import token from "../user.controller";
+
+class Statics {
+  getStatics(req: Request, res: Response) {
+    
+  }
+}
+
+export default new Statics();
