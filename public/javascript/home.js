e20c06289f5e8f3dd61a2bb9721636c53a931717
HoangViet1803 <viethenry1803@gmail.com>
2023-03-06 15:37:11 +0700

    pull view

--- public/javascript/home.js
+++
@@ -1,7 +0,0 @@
-function totalMoneyByWallet(event) {
-  let walletName = document.querySelectorAll(".walletName");
-    
-  //   if (event.target.value ==="all-wallet") {
-  //     document.querySelector("#totalMoney").innerHTML =
-  //   }
-}
